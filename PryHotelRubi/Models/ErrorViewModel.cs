de6f62233816217717505277d11cd28c68fa7509
fioreellaa <maria@MARIA>
2024-06-26 22:11:27 -0500

    Agregar archivos de proyecto.

---
+++ PryHotelRubi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PryHotelRubi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PryHotelRubi/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿namespace PryHotelRubi.Models
+{
+    public class Persona
+    {
+        /*CREATE TABLE T_Persona (
+    usuario VARCHAR(50) PRIMARY KEY,
+    nombre VARCHAR(50),
+    apellido VARCHAR(50),
+    fecha_reg DATE,
+    telefono VARCHAR(15),
+    email VARCHAR(100),
+	contrasena varchar(255),
+    DNI VARCHAR(15)
+);*/
+        public String 
+    }
+}
